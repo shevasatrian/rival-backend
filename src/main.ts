88e2ff2e0487f8ae97d650db4a5dbaeab9bf30d5
Sheva Satrian <sheva.sepot@gmail.com>
2026-02-24 09:07:08 +0700

    feat:update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://rival-frontend.vercel.app',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 8000);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
